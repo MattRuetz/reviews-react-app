05702c5d2df2ea6afc3bdad1596561b2ccc0cfe8
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-04 14:29:45 -0500

    number of reviews and average rating shown at top of UI. Calculated using list react State to hook and update automatically when changes occur

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,19 @@
+const FeedbackStats = ({ feedback }) => {
+    // Calulate average of review scores
+    let average =
+        feedback.reduce((acc, cur) => {
+            return acc + cur.rating;
+        }, 0) / feedback.length;
+
+    //limit to 1 decimal place
+    average = average.toFixed(1);
+
+    return (
+        <div className="feedback-stats">
+            <h4>{feedback.length} Reviews</h4>
+            <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+        </div>
+    );
+};
+
+export default FeedbackStats;

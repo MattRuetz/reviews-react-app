7677adfc0e3dc32d9206da605d0f26a7d41647ac
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-04 14:15:24 -0500

    user can delete card from UI with X button -- does not persist -- TODO

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 // Component for entire list of feedback cards
 // props: feedback - Array of all feedback items as React State
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback... yet!</p>;
     }
@@ -13,7 +13,11 @@ const FeedbackList = ({ feedback }) => {
     return (
         <div className="feedback-list">
             {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} />
+                <FeedbackItem
+                    key={item.id}
+                    item={item}
+                    handleDelete={handleDelete}
+                />
             ))}
         </div>
     );
